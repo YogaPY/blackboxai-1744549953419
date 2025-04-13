a45df5a16dc5c79dbb02c268cf17b359112e9157
YogaPY <yogaprisma@unipma.ac.id>
2025-04-13 20:13:06 +0700

    Add my-hijab/admin/logout.php 1744549993377

---
+++ my-hijab/admin/logout.php
@@ -0,0 +1,12 @@
+<?php
+require_once '../includes/config.php';
+require_once '../includes/functions.php';
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to login page
+redirect('index.php');
